f9e5e39fc4ebfab099607052a6cfa0908524d8c8
Ross Moug <ross.moug@solarwinds.com>
2020-02-19 12:14:50 +0000

    Subscriptions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {fs} = require('file-system');
-const {ApolloServer, gql} = require('apollo-server');
+const {ApolloServer, gql, PubSub, withFilter} = require('apollo-server');
 const {AuthenticationError, UserInputError} = require('apollo-server-errors');
 const {RedisCache} = require('apollo-server-cache-redis');
 const {merge} = require('lodash');
@@ -7,6 +7,25 @@ const {merge} = require('lodash');
 const Author = require('./author');
 const MoviesApi = require('./movies-api');
 const PersonalisationApi = require('./personalisation-api');
+const Post = require('./post');
+
+const COMMENT_ADDED = 'COMMENT_ADDED';
+const POST_ADDED = 'POST_ADDED';
+const pubSub = new PubSub();
+
+const validateToken = authToken => {
+  // ... validate token and return a Promise, rejects in case of an error
+  return true;
+};
+
+const findUser = authToken => {
+  return tokenValidationResult => {
+    // ... finds user by auth token and return a Promise, rejects in case of an error
+    return {
+      username: 'test user',
+    };
+  };
+};
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -26,20 +45,53 @@ const typeDefs = gql`
         title: String
     }
 
+    type Post {
+        author: String
+        comment: String
+    }
+
+    type Comment {
+        id: String
+        content: String
+        repoName: String
+    }
+
+    type Subscription {
+        postAdded: Post
+        commentAdded(repoFullName: String): [Comment]
+    }
+
     type Query {
         getAuthors: [Author]
         movie: Movie
         mostViewedMovies: [Movie],
+        posts: [Post]
         readError: String
         authenticationError: String
     }
 
     type Mutation {
         userInputError(input: String): String
+        addPost(author: String, comment: String): Post
+        addComment(repoName: String, content: String): Comment
     }
 `;
 
 const resolvers = {
+  Subscription: {
+    postAdded: {
+      // Additional event labels can be passed to asyncIterator creation
+      subscribe: () => pubSub.asyncIterator([POST_ADDED]),
+    },
+    commentAdded: {
+      subscribe: withFilter(
+        () => pubSub.asyncIterator(COMMENT_ADDED),
+        (payload, variables) => {
+          return payload.commentAdded.repoName === variables.repoFullName;
+        },
+      ),
+    }
+  },
   Query: {
     getAuthors(parent, args, context, info) {
       return Author.authors;
@@ -49,6 +101,9 @@ const resolvers = {
     },
     mostViewedMovies: async (_source, _args, {dataSources}) => {
       return dataSources.moviesApi.getMostViewedMovies();
+    },
+    posts(root, args, context) {
+      return Post.posts();
     },
     readError: (parent, args, context) => {
       return fs.readFileSync('/non/existent/file');
@@ -64,6 +119,14 @@ const resolvers = {
           invalidArgs: Object.keys(args),
         });
       }
+    },
+    addPost(root, args, context) {
+      pubSub.publish(POST_ADDED, { postAdded: args });
+      return Post.addPost(args);
+    },
+    addComment(root, args, context) {
+      pubSub.publish(COMMENT_ADDED, {commentAdded: args});
+      return null;
     },
   },
 };
@@ -81,9 +144,32 @@ const server = new ApolloServer({
     moviesApi: new MoviesApi.MoviesAPI(),
     personalisationApi: new PersonalisationApi.PersonalizationAPI(),
   }),
-  context: () => ({
-    token: 'foo',
-  }),
+  context: async ({ req, connection }) => {
+    if (connection) {
+      // check connection for metadata
+      return connection.context;
+    } else {
+      // check from req
+      const token = req.headers.authorization || "foo";
+
+      return { token };
+    }
+  },
+  subscriptions: {
+    onConnect: (connectionParams, webSocket) => {
+      if (connectionParams.authToken) {
+        return validateToken(connectionParams.authToken)
+          .then(findUser(connectionParams.authToken))
+          .then(user => {
+            return {
+              currentUser: user,
+            };
+          });
+      }
+
+      // throw new Error('Missing auth token!');
+    },
+  },
   engine: {
     // Rewrite errors before they are sent to Apollo Graph Manager
     rewriteError(err) {
@@ -111,6 +197,7 @@ const server = new ApolloServer({
 });
 
 // The `listen` method launches a web server.
-server.listen().then(({url}) => {
+server.listen().then(({url, subscriptionsUrl}) => {
   console.log(`🚀  Server ready at ${url}`);
+  console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
 });
---
+++ post.js
@@ -0,0 +1,12 @@
+function addPost(post) {
+  console.log("addPost called!");
+}
+
+function posts() {
+  return [];
+}
+
+module.exports = {
+  addPost,
+  posts,
+};
